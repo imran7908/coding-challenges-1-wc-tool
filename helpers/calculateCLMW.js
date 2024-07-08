e1603a35ab27d2106ad0eff88931a10695ee6fe7
imran7908 <imran1510043@gmail.com>
2024-07-08 18:20:38 +0600

    Update log text

--- helpers/calculateCLMW.js
+++ helpers/calculateCLMW.js
@@ -7,9 +7,9 @@ export const splitWords = (contents) => {
 
 export const calculateCLMW = (contents) => {
   const totalLines = contents.split("\n").length - 1;
-  console.log("lines: ", totalLines);
+  console.log("lines(l): ", totalLines);
   const totalWords = splitWords(contents).length;
-  console.log("words: ", totalWords);
+  console.log("words(w): ", totalWords);
   const totalCharacters = contents.split("").length;
-  console.log("bytes: ", totalCharacters);
+  console.log("characters(m): ", totalCharacters);
 };
